3385335f0edd32a35de60c93f05c2afe5c2a3f20
rpranaitis <rolandaspranaitis@gmail.com>
2021-11-12 09:58:39 +0200

    Updated routes

--- routes/weather.js
+++ routes/weather.js
@@ -1,8 +1,35 @@
-let express = require('express');
-let router = express.Router();
+const express = require('express');
+const https = require('https');
+const router = express.Router();
 
-router.get('/', function(req, res, next) {
-	res.send('respond with a resource');
+router.get('/places', function(req, res, next) {
+	https.get('https://api.meteo.lt/v1/places', response => {
+		let result = '';
+
+		response.on('data', data => {
+			result += data.toString();
+		});
+
+		response.on('end', () => {
+			result = JSON.parse(result);
+			result = result.filter(x => x.countryCode === 'LT');
+			res.send(result);
+		});
+	});
+});
+
+router.get('/places/:place', function(req, res, next) {
+	https.get(`https://api.meteo.lt/v1/places/${req.params['place']}/forecasts/long-term`, response => {
+		let result = '';
+
+		response.on('data', data => {
+			result += data.toString();
+		});
+
+		response.on('end', () => {
+			res.send(JSON.parse(result));
+		});
+	});
 });
 
 module.exports = router;
