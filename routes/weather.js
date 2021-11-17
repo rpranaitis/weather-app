56d68737bdd21994875a1a62c9cf1c92fd38ad67
rpranaitis <rolandaspranaitis@gmail.com>
2021-11-17 10:45:32 +0200

    Imported routes

--- routes/weather.js
+++ routes/weather.js
@@ -1,2 +1,7 @@
-require('./places/index');
-require('./places/place');
+const express = require('express');
+const router = express.Router();
+
+router.use('/', require('./places/index'));
+router.use('/', require('./places/place'));
+
+module.exports = router;
