cc695f915463c1add7d9d063130488e01b2bfc8b
rpranaitis <rolandaspranaitis@gmail.com>
2021-11-28 17:50:23 +0200

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -90,7 +90,7 @@ function getFutureDateTimes() {
 
     for (let i = 0; i < 12; i++) {
         let addedHours = new Date().addHours(i);
-        let futureDate = addedHours.toLocaleDateString('lt');
+        let futureDate = addedHours.toLocaleDateString('en-CA');
         let futureTime = addedHours.toLocaleTimeString().slice(0, 2) + ':00:00';
 
         dateTimes.push(`${futureDate} ${futureTime}`)
